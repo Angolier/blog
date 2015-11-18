75c8759010ecb6229d4dc92daaf38c85580d5ac1
brusi <brusi.andrey@gmail.com>
2015-11-18 12:57:56 +0000

    Отредактировал шаблон главной страницы

--- models/articles.php
+++ models/articles.php
@@ -2,10 +2,18 @@
 
 function articles_all() {
     
+    $art1 = ["id" => 1, "title" => "Title1", "date" => "2015-01-01", "content" => "Content1"];
+    $art2 = ["id" => 2, "title" => "Title2", "date" => "2015-01-01", "content" => "Content2"];
+    
+    $arr[0] = $art1;
+    $arr[1] = $art2;
+    
+    return $arr;
 }
 
 function articles_get($id) {
     
+    return ["id" => 1, "title" => "Тестовый заголовок", "date" => "2015-01-01", "content" => "Тестовая статья"];
 }
 
 function articles_new($title, $date, $content) {
