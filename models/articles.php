4238a32918cf15ed541b71d57c8b5a44bb5beaf7
brusi <brusi.andrey@gmail.com>
2015-11-18 18:53:32 +0000

    Добавил взаимодействие с базой для функции articles_all и articles_get

--- models/articles.php
+++ models/articles.php
@@ -1,19 +1,35 @@
 <?php
 
-function articles_all() {
+function articles_all($link)
+{
     
-    $art1 = ["id" => 1, "title" => "Title1", "date" => "2015-01-01", "content" => "Content1"];
-    $art2 = ["id" => 2, "title" => "Title2", "date" => "2015-01-01", "content" => "Content2"];
+    $query = "SELECT * FROM articles ORDER BY id DESC";
+    $result = mysqli_query($link, $query);
     
-    $arr[0] = $art1;
-    $arr[1] = $art2;
+    if (!$result) die(mysqli_error($link));
     
-    return $arr;
+    $n = mysqli_num_rows($result);
+    $articles = array();
+    
+    for ($i = 0; $i < $n; $i++)
+    {
+        $row = mysqli_fetch_assoc($result);
+        $articles[] = $row;
+    }
+    return $articles;
 }
 
-function articles_get($id) {
+function articles_get($link, $id_article) 
+{
     
-    return ["id" => 1, "title" => "Тестовый заголовок", "date" => "2015-01-01", "content" => "Тестовая статья"];
+    $query = sprintf("SELECT * FROM articles WHERE id=%d", (int)$id_article);
+    $result = mysqli_query($link, $query);
+    
+    if (!$result) die(mysqli_error($link));
+    
+    $article = mysqli_fetch_assoc($result);
+    
+    return $article;
 }
 
 function articles_new($title, $date, $content) {
