b27083e03064720ff918c3265a31e9395e7af4bc
brusi <brusi.andrey@gmail.com>
2015-11-19 13:08:26 +0000

    Добавил вывод статьи методом из бд.

--- views/articles.php
+++ views/articles.php
@@ -2,19 +2,21 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Собственный написанный блог</title>
         <link href="css/style.css" rel="stylesheet"> 
+        <title>Собственный написанный блог</title>
     </head>
     <body>
         <header>
             <!--Тут будет логотип и меню-->
         </header>
         <main>
+            <!--<a href="admin">Панель администратора</a>-->
             <?php foreach($articles as $a): ?>
             <article class="post">
                 <header>
-                    <h1><a href="article.php?id=<?= $a['id'] ?>"> <?= $a['title'] ?> </a></h1>
+                    <h1><a href="article.php?id=<?= $a['id'] ?>"><?= $a['title'] ?> </a></h1>
                     <em>Опубликовано: <?= $a['date'] ?></em>
+                    
                 </header>
                 <!--<section class="intro">
                     <p>
@@ -24,7 +26,7 @@
                 <section class="content">
                     <h2>Очередной заголовок</h2>-->
                     <p>
-                        <?= $a['content'] ?>
+                        <?= articles_intro($a['content']) ?>
                     </p>
                 </section>
             </article>
